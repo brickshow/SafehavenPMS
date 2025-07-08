c480064ecb7205fa116823aca8af0acc730fa86a
act.blampago@gmail.com <act.blampago@gmail.com>
2025-07-08 15:56:49 +0800

    Updated From Mainlab (Email)

---
+++ SafehavenPMS/Controllers/EmailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SafehavenPMS.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
