9ef5d4447979d428a91d754a150b889451be8c4a
brickshow <act.blampago@gmail.com>
2025-07-15 15:42:54 +0800

    Adding new patients

---
+++ SafehavenPMS/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SafehavenPMS.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Add user step1 
+
+        public IActionResult NewUser()
+        {
+            return View();
+        }
+    }
+}
