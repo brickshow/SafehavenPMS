2acfab59bb6f75bc8c0c4b647c602867125476e4
brickshow <act.blampago@gmail.com>
2025-09-23 13:51:20 +0800

    asasdsdasd

---
+++ SafehavenPMS/ViewModel/Billing/MiscellaneousItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SafehavenPMS.ViewModel.Billing
+{
+    public class MiscellaneousItemViewModel
+    {
+        public int PatientId { get; set; }
+        public List<string> ItemDescriptions { get; set; }
+        public List<decimal> Amounts { get; set; }
+        public decimal Total { get; set; }
+    }
+}
